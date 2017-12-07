c4bbfe3ae84a3db32fd6618fcea611760b5e14a6
KimGenius <geniusk1047@naver.com>
2017-12-07 12:37:41 +0900

    package name 수정:

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 module.exports = {
   // Map of hooks
   hooks: {},
-  book: {
-    assets: './assets',
-    css: [
-      'codetabs.css'
-    ],
-    js: [
-      'codetabs.js'
-    ]
-  },
   // Map of new blocks
   blocks: {
     code-path: {
       blocks: ['code-path'],
-      process: function (parentBlock) {
-        var blocks = [parentBlock].concat(parentBlock.blocks)
-        return '<b>' + blocks + '</b>'
+      process: function (block) {
+        return '<b>' + block.body + '</b>'
       }
     }
   }
