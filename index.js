cb13f7431d35216b34b20bb814338200701a0d72
KimGenius <geniusk1047@naver.com>
2017-12-07 17:37:12 +0900

    39번째 도전

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = {
   hooks: {
     "page": function (page) {
       page.content = page.content
-        .replace(/{/gi, '</pre><pre><var><span style="color:#ec407a;font-weight:bold;font-style:italic;">')
-        .replace(/}/gi, '</span></var></pre><pre>')
+        .replace(/{/gi, '</code><code><var><span style="color:#ec407a;font-weight:bold;font-style:italic;">')
+        .replace(/}/gi, '</span></var></code><code>')
       return page;
     }
   }
