65cf7c6e9f515368549c6deb08bba9377074b68f
KimGenius <geniusk1047@naver.com>
2017-12-07 12:56:14 +0900

    => error 수정

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ module.exports = {
       process: function (block) {
         const splitStr = '+pp+'
         const result = '<pre>'
-        block.split(splitStr).forEach((e, i){
-          if (i === 0) {
-            result += e
-          } else {
-            result += '<span style="color:#ec407a; font-weight:bold; font-style: italic;">' + e + '</span>'
+        block.split(splitStr).forEach((e, i) => {
+            if (i === 0) {
+              result += e
+            } else {
+              result += '<span style="color:#ec407a; font-weight:bold; font-style: italic;">' + e + '</span>'
+            }
           }
-        })
+        )
         result += '</pre>'
         return result
       }
