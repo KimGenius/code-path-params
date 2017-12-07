72c8ca9802a953fe51894bdc33c4a8e50b21a74a
KimGenius <geniusk1047@naver.com>
2017-12-07 18:11:57 +0900

    와 드디어 됐다

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 module.exports = {
-  // Map of hooks
   hooks: {
     "page": function (page) {
       page.content = page.content
+        //code태그를 쓰면 안에 내용이 다 적용이 안되므로 바꿔줘야함
         .replace("<code>", "<span>")
         .replace("</code>", "</span>")
         .replace(/{/gi, '<var><span style="color:#ec407a;font-weight:bold;font-style:italic;">{')
