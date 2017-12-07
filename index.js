b17118e498f383f3e28ef3f7eef9725fa135c922
KimGenius <geniusk1047@naver.com>
2017-12-07 12:33:47 +0900

    books폴더는 어디에 있는거지?

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 module.exports = {
-    // Map of hooks
-    hooks: {},
-
-    // Map of new blocks
-    blocks: {},
-
-    // Map of new filters
-    filters: {}
-};
+  // Map of hooks
+  hooks: {},
+  book: {
+    assets: './assets',
+    css: [
+      'codetabs.css'
+    ],
+    js: [
+      'codetabs.js'
+    ]
+  },
+  // Map of new blocks
+  blocks: {
+    code-path: {
+      blocks: ['code-path'],
+      process: function (parentBlock) {
+        var blocks = [parentBlock].concat(parentBlock.blocks)
+        return '<b>' + blocks + '</b>'
+      }
+    }
+  }
+  // Map of new filters
+  filters: {}
+}
