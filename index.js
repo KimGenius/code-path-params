b9f3c940a8ef0d9a76474fbccf5db0635daf2e0d
KimGenius <geniusk1047@naver.com>
2017-12-08 10:03:14 +0900

    원래대로

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = {
   hooks: {
     "page": function (page) {
       page.content = page.content
-      //code태그를 쓰면 안에 내용이 다 적용이 안되므로 바꿔줘야함
-        .replace(/<code>/gi, "<span>")
-        .replace(/<\/code>/gi, "</span>")
+        //code태그를 쓰면 안에 내용이 다 적용이 안되므로 바꿔줘야함
+        .replace("<code>", "<span>")
+        .replace("</code>", "</span>")
         .replace(/{/gi, '<var><span style="color:#ec407a;font-weight:bold;font-style:italic;">{')
         .replace(/}/gi, '}</span></var>')
       return page;
