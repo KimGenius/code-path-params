0bb45a418648ff3c755f05df22bcee49797364a3
KimGenius <geniusk1047@naver.com>
2017-12-27 16:31:51 +0900

    log 테스트

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ module.exports = {
         //code태그를 쓰면 안에 내용이 다 적용이 안되므로 바꿔줘야함
         .replace("<code>", "<span>")
         .replace("</code>", "</span>")
-        .replace(/{/gi, '<var><span style="color:#ec407a;font-weight:bold;font-style:italic;"><span>\{</span>')
-        .replace(/}/gi, '<span>\}</span></span></var>')
+        .replace(/{/gi, '<var><span style="color:#ec407a;font-weight:bold;font-style:italic;"><span>{</span>')
+        .replace(/}/gi, '<span>}</span></span></var>')
+      console.log("test :: ",page.content)
       return page;
     }
   }
