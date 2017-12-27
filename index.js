b89012d76692c6f16fc700c62cf2a31a01cb8d1b
KimGenius <geniusk1047@naver.com>
2017-12-27 18:37:16 +0900

    Edit regex

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = {
         //code태그를 쓰면 안에 내용이 다 적용이 안되므로 바꿔줘야함
         .replace("<code>", "<span>")
         .replace("</code>", "</span>")
-        .replace(/{/gi, '<var><span style="color:#ec407a;font-weight:bold;font-style:italic;"><span>{</span>')
-        .replace(/}/gi, '<span>}</span></span></var>')
+        .replace(/{/g, '<var><span style="color:#ec407a;font-weight:bold;font-style:italic;"><span>{</span>')
+        .replace(/}/g, '<span>}</span></span></var>')
       console.log("test :: ",page.content)
       return page;
     }
